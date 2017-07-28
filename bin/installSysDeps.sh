d2729f0175492dbc3d993b7f972cfc0b57245a1d
陈晔 <chenye94@qq.com>
2017-07-28 10:37:13 -0700

    deb improve

--- bin/installSysDeps.sh
+++ bin/installSysDeps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y unzip gzip git curl python libssl-dev pkg-config build-essential 
+sudo apt-get install -y unzip gzip git curl python libssl-dev pkg-config build-essential mongodb
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - 
 sudo apt-get install -y nodejs
