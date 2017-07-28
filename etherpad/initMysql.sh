0bd37998a93fc783bf237f9d19dfbc22bde9ea7b
陈晔 <chenye94@qq.com>
2017-07-28 10:18:20 -0700

    deb improve

--- etherpad/initMysql.sh
+++ etherpad/initMysql.sh
@@ -7,7 +7,7 @@ EP_DB="etherpad-lite"
 EP_USER="etherpad"
 EP_PWD="cloudfortep"
 
-MYSQL_SCRIPT="create database \`${EP_DB}\`;"
+MYSQL_SCRIPT="CREATE DATABASE IF NOT EXISTS \`${EP_DB}\`;"
 MYSQL_SCRIPT="$MYSQL_SCRIPT grant CREATE,ALTER,SELECT,INSERT,UPDATE,DELETE on \`${EP_DB}\`.* to '${EP_USER}'@'localhost' identified by '${EP_PWD}';"
 
 mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PWD <<< "$MYSQL_SCRIPT"
